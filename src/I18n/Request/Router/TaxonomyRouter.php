b2be7c24b373595cb8bf6908c3bc789cd883c8ee
Francois Faubert <francois.faubert@iprospect.com>
2016-04-18 09:33:58 -0400

    translated routed term urls trees

--- src/I18n/Request/Router/TaxonomyRouter.php
+++ src/I18n/Request/Router/TaxonomyRouter.php
@@ -10,15 +10,26 @@ class TaxonomyRouter extends PolyglotRouter {
     {
         global $wp_query;
 
-        if ($wp_query->queried_object) {
-            $term = $wp_query->queried_object;
-            $localizedTerm = $this->currentLocale->getTranslatedTerm($term->term_id, $term->taxonomy);
-            $originalTerm = $this->defaultLocale->getTranslatedTerm($term->term_id, $term->taxonomy);
-            $route = $this->replaceFirstOccurance($localizedTerm->slug, $originalTerm->slug, $route);
-            return $this->replaceFirstOccurance($this->currentLocale->getHomeUrl(false), "/", $route);
+        if ($wp_query->queried_object && get_class($wp_query->queried_object) === "WP_Term") {
+            return $this->replaceTermLevel($wp_query->queried_object, $route);
         }
 
         return $route;
     }
 
+    protected function replaceTermLevel(WP_Term $term, $route)
+    {
+        $localizedTerm = $this->currentLocale->getTranslatedTerm($term->term_id, $term->taxonomy);
+        $originalTerm = $this->defaultLocale->getTranslatedTerm($term->term_id, $term->taxonomy);
+
+        $route = $this->replaceFirstOccurance($localizedTerm->slug, $originalTerm->slug, $route);
+        $localizedRoute = $this->replaceFirstOccurance($this->currentLocale->getHomeUrl(false), "/", $route);
+
+        if ((int)$originalTerm->parent > 0) {
+            $originalParentTerm = $this->defaultLocale->getTranslatedTerm($term->parent, $term->taxonomy);
+            $localizedRoute = $this->replaceTermLevel($originalParentTerm, $localizedRoute);
+        }
+
+        return $localizedRoute;
+    }
 }
